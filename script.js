85e46e86687c91f773d1304b3ebac78855cf691d
Gabriel Osuobiem <osuobiem@gmail.com>
2021-12-28 01:00:39 +0100

    Recalculate minute and hour hand angles

--- script.js
+++ script.js
@@ -33,59 +33,60 @@ for (let i = 1; i <= 12; i++) {
     clockNumbers.appendChild(clockNumber);
 }
 
-// Rotate Second Hand
+// Rotate Clock Hands & Set Digital Watch
+
+// Seconds
 let secondHandAngle = date.getSeconds() * 6;
 
 let secondsCount = date.getSeconds();
 seconds.innerText = secondsCount.toString().length > 1 ? secondsCount : `0${secondsCount}`;
 
-setInterval(() => {
-    secondHandAngle += 6;
-    secondHand.setAttribute('style', `transform: rotate(${secondHandAngle}deg)`);
-
-    secondsCount = (secondsCount === 59) ? 0 : secondsCount + 1;
-    seconds.innerText = secondsCount.toString().length > 1 ? secondsCount : `0${secondsCount}`;
-
-    playSound ? tickSound.play() : null;
-}, 1000);
-
-
-// Rotate Minute Hand
+// Minutes
 let minuteHandAngle = date.getMinutes() * 6;
 minuteHand.setAttribute('style', `transform: rotate(${minuteHandAngle}deg)`);
 
 let minuteCount = date.getMinutes();
 minute.innerText = minuteCount.toString().length > 1 ? minuteCount : `0${minuteCount}`;
 
-setInterval(() => {
-    minuteHandAngle += secondHandAngle / (60 * 60);
-    minuteHand.setAttribute('style', `transform: rotate(${minuteHandAngle}deg)`);
-
-    if (secondsCount === 0) {
-        minuteCount = (new Date()).getMinutes();
-        minute.innerText = minuteCount.toString().length > 1 ? minuteCount : `0${minuteCount}`;
-    }
-}, 1000);
-
-
-// Rotate Hour Hand
-let hourHandAngle = (date.getHours() * 30) + (minuteHandAngle / 12);
+// Hours
+let hourHandAngle = ((date.getHours() % 12) * 30) + (minuteCount / 2);
 hourHand.setAttribute('style', `transform: rotate(${hourHandAngle}deg)`);
 
 let hourCount = date.getHours();
 hour.innerText = hour.innerText = hourCount.toString().length > 1 ? hourCount : `0${hourCount}`;
 
-setInterval(() => {
-    hourHandAngle = (date.getHours() * 30) + (minuteHandAngle / 12);
-    hourHand.setAttribute('style', `transform: rotate(${hourHandAngle}deg)`);
 
-    if (minuteCount === 0) {
-        hourCount = (new Date()).getHours();
-        hour.innerText = hourCount.toString().length > 1 ? hourCount : `0${hourCount}`;
+setInterval(() => {
+    // Seconds
+    secondHandAngle += 6;
+    secondHand.setAttribute('style', `transform: rotate(${secondHandAngle}deg)`);
+
+    secondsCount = (secondsCount === 59) ? 0 : secondsCount + 1;
+    seconds.innerText = secondsCount.toString().length > 1 ? secondsCount : `0${secondsCount}`;
+
+    // Minutes
+    if (secondsCount === 0) {
+        minuteHandAngle += 6;
+        minuteHand.setAttribute('style', `transform: rotate(${minuteHandAngle}deg)`);
+
+        minuteCount = (new Date()).getMinutes();
+        minute.innerText = minuteCount.toString().length > 1 ? minuteCount : `0${minuteCount}`;
     }
 
+    // Hours
+    if (minuteCount === 0) {
+        hourCount = (new Date()).getHours()
+        hour.innerText = hourCount.toString().length > 1 ? hourCount : `0${hourCount}`;
+
+        hourHandAngle = (((new Date()).getHours() % 12) * 30) + (minuteCount / 2);
+        hourHand.setAttribute('style', `transform: rotate(${hourHandAngle}deg)`);
+    }
+    
+    // Days
     if (hourCount === 0) {
         newDate = (new Date()).getDate();
         day.innerText = newDate.toString().length > 1 ? newDate : `0${newDate}`;
     }
+
+    playSound ? tickSound.play() : null;
 }, 1000);
